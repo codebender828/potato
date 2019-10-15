0827fbdb2728326f10b1bf97742be1a6638da3de
codebender828 <jbakebwa@163.com>
2019-10-16 02:12:04 +0800

    i have prettier

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -50,44 +50,44 @@ export default {
   name: "Editor",
   data() {
     const code = `
-      <template>
-        <h1>Hello World!</h1>
-        <codemirror v-model="code" :options="cmOption"></codemirror>
-      </template>
-      <script>
-        // import 'some-codemirror-resource'
-        export default {
-          data() {
-            return {
-              code: 'const A = 10',
-              cmOption: {
-                tabSize: 4,
-                styleActiveLine: true,
-                lineNumbers: true,
-                line: true,
-                foldGutter: true,
-                styleSelectedText: true,
-                mode: 'text/javascript',
-                keyMap: "sublime",
-                matchBrackets: true,
-                showCursorWhenSelecting: true,
-                theme: "monokai",
-                extraKeys: { "Ctrl": "autocomplete" },
-                hintOptions:{
-                  completeSingle: false
-                }
-              }
-            }
+<template>
+  <h1>Hello World!</h1>
+  <codemirror v-model="code" :options="cmOption"></codemirror>
+</template>
+<script>
+  // import 'some-codemirror-resource'
+  export default {
+    data() {
+      return {
+        code: 'const A = 10',
+        cmOption: {
+          tabSize: 4,
+          styleActiveLine: true,
+          lineNumbers: true,
+          line: true,
+          foldGutter: true,
+          styleSelectedText: true,
+          mode: 'text/javascript',
+          keyMap: "sublime",
+          matchBrackets: true,
+          showCursorWhenSelecting: true,
+          theme: "monokai",
+          extraKeys: { "Ctrl": "autocomplete" },
+          hintOptions:{
+            completeSingle: false
           }
         }
-      <\/script>
-      <style lang="scss">
-        @import './sass/mixins';
-        @import './sass/variables';
-        main {
-          position: relative;
-        }
-      </style>
+      }
+    }
+  }
+<\/script>
+<style lang="scss">
+  @import './sass/mixins';
+  @import './sass/variables';
+  main {
+    position: relative;
+  }
+</style>
     `;
     return {
       code,
